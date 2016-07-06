24ada1df12b991c2b18c93ade083097ce4285871
Andrew Haust <andrewwhhaust@gmail.com>
2016-07-05 21:34:01 -0400

    Fix pager helper

--- app/helpers/pager_helper.rb
+++ app/helpers/pager_helper.rb
@@ -2,9 +2,9 @@ module PagerHelper
   def paginate(total, current)
     elements = (1..total).map do |page|
       if page == current
-        content_tag :div, page, class: 'page current'
+        content_tag :div, content_tag(:span, page), class: 'page current control'
       else
-        content_tag :div, link_to(page, root_path(page: page)), class: 'page current'
+        content_tag :div, link_to(page, root_path(page: page)), class: 'page control'
       end
     end
 
